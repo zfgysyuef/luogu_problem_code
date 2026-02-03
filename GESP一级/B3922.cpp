f907605470e5ddd8d88a57e9cf32afac7e28c1d4
zzkkkz <129380099+zfgysyuef@users.noreply.github.com>
2026-02-04 07:24:05 +0800

    Create B3922.cpp

---
+++ GESP一级/B3922.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    for (int i = 1 ; i <= n ; i ++){
+        if(i%m!=0){
+            cout << i << endl;
+        }
+    }
+}
